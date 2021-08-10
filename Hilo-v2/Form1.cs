07032cb5d6d2a061823ec316b2ec42b1cde9ce5d
poky1084 <nanohtcop@gmail.com>
2021-08-10 20:40:51 +0300

    Removed code

--- Hilo-v2/Form1.cs
+++ Hilo-v2/Form1.cs
@@ -1,20 +1,14 @@
 ﻿using RestSharp;
 using Newtonsoft.Json;
-using RestSharp.Deserializers;
 using System;
-using System.Diagnostics;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Runtime.InteropServices;
 using Newtonsoft.Json.Serialization;
 using System.Net;
-using System.Media;
+
 
 namespace Hilo_v2
 {
