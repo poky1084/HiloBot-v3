10a2ac574893ea63152c134e809fa081d3d20b02
poky1084 <nanohtcop@gmail.com>
2021-08-17 10:11:33 +0200

    Icon 32x32

--- Hilo-v2/Form1.Designer.cs
+++ Hilo-v2/Form1.Designer.cs
@@ -2046,7 +2046,7 @@ namespace Hilo_v2
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            
             this.Name = "Form1";
             this.Text = "HiLo";
             this.Load += new System.EventHandler(this.Form1_Load);
